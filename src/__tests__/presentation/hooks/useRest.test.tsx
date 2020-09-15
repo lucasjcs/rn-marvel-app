6479a2638f36d3b09f4c6e682a242aeb2552ab08
lucasjcs <silval@alunos.utfpr.edu.br>
2020-09-15 02:39:32 -0300

    test: adicionando teste de falha de api

--- src/__tests__/presentation/hooks/useRest.test.tsx
+++ src/__tests__/presentation/hooks/useRest.test.tsx
@@ -11,7 +11,7 @@ import { BaseResponse } from '@/helpers/models/BaseResponse';
 const apiMock = new MockAdapter(api);
 
 describe('useRest', () => {
-  it('should get from api using rest hook', async () => {
+  it('should get from api success', async () => {
     apiMock.onGet('/character').reply(200, {
       data: {
         results: characterList,
@@ -26,5 +26,21 @@ describe('useRest', () => {
     }));
 
     expect(result.current[0].data?.data.results?.length).toBe(3);
+    expect(result.current[0].loading).toBeFalsy();
+    expect(result.current[0].error).toBeUndefined();
+  });
+
+  it('should get from api failure', async () => {
+    apiMock.onGet('/character').reply(500);
+
+    const { result } = renderHook(() => useRest<BaseResponse>('/character'));
+
+    await act(() => result.current[1]({
+      limit: 1,
+      offset: 1,
+    }));
+
+    expect(result.current[0].error).not.toBeUndefined();
+    expect(result.current[0].loading).toBeFalsy();
   });
 });
